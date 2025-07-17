f60a474115b2a301203c0871bb73bd61b5c8cbae
Daniel Molloy <danielmolloy_6@icloud.com>
2025-07-17 14:20:51 +0100

    updated cors

--- practice_journal/settings.py
+++ practice_journal/settings.py
@@ -74,13 +74,13 @@ MIDDLEWARE = [
 
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:3000",
-    "jam-jar-frontend-danmolloys-projects.vercel.app",
+    "https://jam-jar-frontend-danmolloys-projects.vercel.app",
 
 ]
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",  
-    "jam-jar-frontend-danmolloys-projects.vercel.app",
+    "https://jam-jar-frontend-danmolloys-projects.vercel.app",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
